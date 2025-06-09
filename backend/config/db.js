520487720392f8be6d1f260d70c4488c30a576ae
Naufal Septrio Akbar <naufalseptrioakbar557@gmail.com>
2025-06-10 01:24:05 +0700

    Ubah  connection db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  timezone: "+07:00",
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
