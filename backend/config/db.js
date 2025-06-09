ea68c237fdd4b39af5e50cd246924827149bd08a
Naufal Septrio Akbar <naufalseptrioakbar557@gmail.com>
2025-06-10 01:30:37 +0700

    kembali ke awal

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,14 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  timezone: "Z",
+});
 
 db.connect((err) => {
   if (err) {
